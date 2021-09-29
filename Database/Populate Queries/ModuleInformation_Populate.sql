75b4795083f5feb8d85e5041930b33cca7df88e1
HeleneConradie <heleneconradie01@gmail.com>
2021-09-29 22:46:36 +0200

    Created Queries & New Database Backup
    Co-Authored-By: Morras01 <91467956+Morras01@users.noreply.github.com>

---
+++ Database/Populate Queries/ModuleInformation_Populate.sql
@@ -0,0 +1,29 @@
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('DBD281', 'Database Development', 'The student will learn to use the different tools and techniques available for the administration and 
+maintenance of a relational database system.','https://www.youtube.com/watch?v=moCgS2kfxnU');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('INF281', 'Information Systems', 'This module will further investigate security and ethical considerations 
+for public, private and hybrid cloud services and delivery models.', 'https://www.youtube.com/watch?v=M988_fsOSWo');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('INL201', 'Innovation And Leadership', ' This module prepares the student to function effectively as a team leader.', 'https://www.youtube.com/watch?v=vb5elHUqfWg');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('INL202', 'Innovation And Leadership', 'This module prepares the student to act as a specialist or consultant for a specific body of 
+knowledge.', 'https://www.youtube.com/watch?v=cAdyB0h3Ltc');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('LPR281', 'Linear Programming', 'Linear Programming is a scientific approach to decision making that seeks to best design and operate 
+a system.', 'https://www.youtube.com/watch?v=Wn45puC08DA');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('MAT281', 'Mathematics', 'This module focuses on developing an understanding of essential mathematical principles, 
+mathematical thinking skills and reasoning.', 'https://www.youtube.com/watch?v=9AI3BkKQhn0');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('PRG281', 'Programming', 'The course will focus on Windows Forms and 
+students will learn how to create professional aesthetics and interactivity.', 'https://www.youtube.com/watch?v=SiBw7os-_zI');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('PRG282', 'Programming', 'Students will be able to implement the basic concepts and technologies involved in working 
+with files.', 'https://www.youtube.com/watch?v=TU1rTuBM1Cs');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('PMM281', 'Project Management', 'The course will provide the student with the means of conducting the management of a project.', 'https://www.youtube.com/watch?v=rq5nedNYaXE');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('STA281', 'Statistics', 'The overall purpose of the program is to produce graduates that can think clearly and critically.', 'https://www.youtube.com/watch?v=z0Ry_3_qhDw');
+
+insert into ModuleInformation (ModuleCode, ModuleName, ModuleDescription, OnlineResources) values ('WPR281', 'Web Programming', 'The purpose of the course is to introduce interactive and dynamic web design by incorporating a 
+programming language into a web page.', 'https://www.youtube.com/watch?v=1HakS7KsbCk');
