12ded0c09641714d3f3d6f370d180906a03a525f
HeleneConradie <heleneconradie01@gmail.com>
2021-09-30 21:55:58 +0200

    Created Data Access Layer & Classes Within Layer

---
+++ DataSmart/DataSmart/Data Access Layer/DH_WriteTextFiles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSmart.Data_Access_Layer
+{
+    class DH_WriteTextFiles
+    {
+    }
+}
