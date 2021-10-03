bdc5f91168b5e1cabecfc6d44b99cb51052f1f21
HeleneConradie <heleneconradie01@gmail.com>
2021-10-03 15:06:09 +0200

    Created Database Connection Class
    Linked the database to class

--- DataSmart/DataSmart/Data Access Layer/DH_DatabaseConnection.cs
+++ DataSmart/DataSmart/Data Access Layer/DH_DatabaseConnection.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DataSmart.Data_Access_Layer
 {
     class DH_DatabaseConnection
     {
+        private bool connected = false;
+
+        private readonly string connection = @"Data Source = .; Intial Catalog = dbdDataSmart; Integrated Security = SSPI";
+
+        public string connect { get => connection; }
+
+        private void Con (string connect) 
+        {
+            using (SqlConnection c = new SqlConnection(connect))
+            {
+                try
+                {
+                    c.Open();
+                    connected = true;
+                }
+                catch (SqlException exc)
+                {
+                    connected = false;
+                    throw new Exception(exc.Message);
+                }
+            }
+        }
     }
 }
