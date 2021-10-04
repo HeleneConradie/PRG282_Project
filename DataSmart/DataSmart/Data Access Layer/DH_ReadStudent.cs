3e71d42c5092af2803279aa0857160109acdc146
Morras01 <mornejvrensburg4@gmail.com>
2021-10-04 15:18:29 +0200

    Completed ReadStudent and WriteModifyStudent classes

--- DataSmart/DataSmart/Data Access Layer/DH_ReadStudent.cs
+++ DataSmart/DataSmart/Data Access Layer/DH_ReadStudent.cs
@@ -3,10 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace DataSmart.Data_Access_Layer
 {
     class DH_ReadStudent
     {
+        DH_DatabaseConnection dbHandler = new DH_DatabaseConnection();
+
+        public List<Student> ReadAll()
+        {
+            try
+            {
+                List<Student> AllStudents = new List<Student>();
+                SqlConnection connection = new SqlConnection(dbHandler.connect);
+                string Query = "SELECT * FROM StudentInformation";
+                SqlCommand cmd = new SqlCommand(Query, connection);
+                SqlDataReader dr;
+                connection.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    AllStudents.Add(new Student(
+                        dr.GetValue(0).ToString(),
+                        dr.GetValue(1).ToString(),
+                        dr.GetValue(2).ToString(),
+                        dr.GetValue(3).ToString(),
+                        dr.GetValue(4).ToString(),
+                        dr.GetValue(5).ToString(),
+                        dr.GetValue(6).ToString(),
+                        dr.GetValue(7).ToString()
+                        ));
+                }
+                connection.Close();
+                return AllStudents;
+            }
+            catch (SqlException SqlEx)
+            {
+                MessageBox.Show(SqlEx.Message);
+                return null;
+            }
+        }
+
+        public string ReadStudent(string StudentNumber)
+        {
+            string value = "";
+            try
+            {
+                SqlConnection connection = new SqlConnection(dbHandler.connect);
+                string Query = "SELECT * FROM StudentInformation WHERE StudentNumber ='" + StudentNumber + "'";
+                SqlCommand cmd = new SqlCommand(Query, connection);
+                SqlDataReader dr;
+                connection.Open();
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    value = dr.GetValue(0) + "#" + dr.GetValue(1) + "#" + dr.GetValue(2) + "#" + dr.GetValue(3) + "#" + dr.GetValue(4) + "#" + dr.GetValue(5) + "#" + dr.GetValue(6) + "#" + dr.GetValue(7);
+                }
+                connection.Close();
+                return value;
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+                return null;
+            }
+        }
     }
 }
--- DataSmart/DataSmart/Data Access Layer/DH_WriteModifyStudent.cs
+++ DataSmart/DataSmart/Data Access Layer/DH_WriteModifyStudent.cs
@@ -3,10 +3,118 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DataSmart.Data_Access_Layer
 {
     class DH_WriteModifyStudent
     {
+        DH_DatabaseConnection dbHandler = new DH_DatabaseConnection();
+
+        public bool InsertStudent(Student objStu)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(dbHandler.connect);
+                string Query = string.Format(@"INSERT INTO StudentInformation(StudentNumber, StudentFirtsName, StudentLastName, StudentImage, StudentDateOfBirth, StudentGender, StudentPhone, StudentAddress) Values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', {7}')",
+                    objStu.StudentNumber,
+                    objStu.StudentFirstName,
+                    objStu.StudentLastName,
+                    objStu.StudentImage,
+                    objStu.StudentDateOfBirth,
+                    objStu.StudentGender,
+                    objStu.StudentPhone,
+                    objStu.StudentAddress);
+                SqlCommand command = new SqlCommand(Query, con);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateStudentImage(string StuNum, string StuImg)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(dbHandler.connect);
+                string Query = "UPDATE StudentInformation SET StudentImage = '" + StuImg + "' WHERE StudentNumber = '" + StuNum + "'";
+                SqlCommand command = new SqlCommand(Query, con);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateStudentPhone(string StuNum, string StuPhone)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(dbHandler.connect);
+                string Query = "UPDATE StudentInformation SET StudentPhone = '" + StuPhone + "' WHERE StudentNumber = '" + StuNum + "'";
+                SqlCommand command = new SqlCommand(Query, con);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateStudentAddress(string StuNum, string StuAddress)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(dbHandler.connect);
+                string Query = "UPDATE StudentInformation SET StudentImage = '" + StuAddress + "' WHERE StudentNumber = '" + StuNum + "'";
+                SqlCommand command = new SqlCommand(Query, con);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteStudent(string StuNum)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(dbHandler.connect);
+                string Query = "DELETE FROM StudentInformation WHERE StudentNumber = '" + StuNum + "'";
+                SqlCommand command = new SqlCommand(Query, con);
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+                return true;
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message);
+                return false;
+            }
+        }
     }
 }
+
+
