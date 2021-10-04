3de0c1c0c7d93b8adab1d24d66112f25734b4eee
Henco <hencodelange123@gmail.com>
2021-10-04 11:38:25 +0200

    Created to Read Login and WriteLogin

--- DataSmart/DataSmart/Data Access Layer/DH_ReadLogin.cs
+++ DataSmart/DataSmart/Data Access Layer/DH_ReadLogin.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DataSmart.Data_Access_Layer
 {
     class DH_ReadLogin
     {
+        public List<string> ReadLogin()
+        {
+            try
+            {
+                List<string> Users = new List<string>();
+                string path = @"TextFiles/UserLogin.txt";
+                FileStream fStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                using (StreamReader sReader = new StreamReader(fStream))
+                {
+                    string line;
+                    while ((line = sReader.ReadLine()) != null)
+                    {
+                        Users.Add(line);
+                    }
+                }
+                return Users;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return null;
+            }
+        }
     }
 }
--- DataSmart/DataSmart/Data Access Layer/DH_WriteTextFiles.cs
+++ DataSmart/DataSmart/Data Access Layer/DH_WriteTextFiles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace DataSmart.Data_Access_Layer
 {
     class DH_WriteTextFiles
     {
+        public bool WriteTextFiles(string Login, string Detail)
+        {
+            try
+            {
+                string DetailPath = @"TextFiles/UserDetail.txt";
+                string LoginPath = @"TextFiles/UserLogin.txt";
+                FileStream fStream = new FileStream(LoginPath, FileMode.Append, FileAccess.Write);
+                using (StreamWriter sr = new StreamWriter(fStream))
+                {
+                    sr.WriteLine(Login);
+
+                }
+
+                FileStream fStream1 = new FileStream(DetailPath, FileMode.Append, FileAccess.Write);
+                using (StreamWriter sr = new StreamWriter(fStream1))
+                {
+                    sr.WriteLine(Detail);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
