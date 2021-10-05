68503a4e42e5298c60f975c573d57d3606ce2896
HeleneConradie <heleneconradie01@gmail.com>
2021-10-05 19:36:47 +0200

    Created Business Logic Layer Folder and Classes

---
+++ DataSmart/DataSmart/Business Logic Layer/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSmart.Business_Logic_Layer
+{
+    class Student
+    {
+    }
+}
