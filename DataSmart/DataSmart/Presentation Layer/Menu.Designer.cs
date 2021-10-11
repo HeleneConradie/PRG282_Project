45241991119defa1870c36680713e806c3dd8d18
Morras01 <91467956+Morras01@users.noreply.github.com>
2021-10-11 21:10:51 +0200

    Designed Menu

---
+++ DataSmart/DataSmart/Presentation Layer/Menu.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace DataSmart.Presentation_Layer
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnModule = new System.Windows.Forms.RadioButton();
+            this.rbtnStudent = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbtnModify = new System.Windows.Forms.RadioButton();
+            this.rbtnAdd = new System.Windows.Forms.RadioButton();
+            this.rbtnSearch = new System.Windows.Forms.RadioButton();
+            this.rbtnViewAll = new System.Windows.Forms.RadioButton();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(679, 478);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnContinue);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(124, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(421, 375);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(112, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnStudent);
+            this.groupBox1.Controls.Add(this.rbtnModule);
+            this.groupBox1.Location = new System.Drawing.Point(14, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 115);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbtnModule
+            // 
+            this.rbtnModule.AutoSize = true;
+            this.rbtnModule.Location = new System.Drawing.Point(6, 19);
+            this.rbtnModule.Name = "rbtnModule";
+            this.rbtnModule.Size = new System.Drawing.Size(60, 17);
+            this.rbtnModule.TabIndex = 0;
+            this.rbtnModule.TabStop = true;
+            this.rbtnModule.Text = "Module";
+            this.rbtnModule.UseVisualStyleBackColor = true;
+            this.rbtnModule.CheckedChanged += new System.EventHandler(this.rbtnModule_CheckedChanged);
+            // 
+            // rbtnStudent
+            // 
+            this.rbtnStudent.AutoSize = true;
+            this.rbtnStudent.Location = new System.Drawing.Point(6, 42);
+            this.rbtnStudent.Name = "rbtnStudent";
+            this.rbtnStudent.Size = new System.Drawing.Size(62, 17);
+            this.rbtnStudent.TabIndex = 1;
+            this.rbtnStudent.TabStop = true;
+            this.rbtnStudent.Text = "Student";
+            this.rbtnStudent.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.rbtnViewAll);
+            this.groupBox2.Controls.Add(this.rbtnSearch);
+            this.groupBox2.Controls.Add(this.rbtnAdd);
+            this.groupBox2.Controls.Add(this.rbtnModify);
+            this.groupBox2.Location = new System.Drawing.Point(222, 158);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(180, 115);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // rbtnModify
+            // 
+            this.rbtnModify.AutoSize = true;
+            this.rbtnModify.Location = new System.Drawing.Point(20, 19);
+            this.rbtnModify.Name = "rbtnModify";
+            this.rbtnModify.Size = new System.Drawing.Size(56, 17);
+            this.rbtnModify.TabIndex = 0;
+            this.rbtnModify.TabStop = true;
+            this.rbtnModify.Text = "Modify";
+            this.rbtnModify.UseVisualStyleBackColor = true;
+            // 
+            // rbtnAdd
+            // 
+            this.rbtnAdd.AutoSize = true;
+            this.rbtnAdd.Location = new System.Drawing.Point(20, 43);
+            this.rbtnAdd.Name = "rbtnAdd";
+            this.rbtnAdd.Size = new System.Drawing.Size(44, 17);
+            this.rbtnAdd.TabIndex = 1;
+            this.rbtnAdd.TabStop = true;
+            this.rbtnAdd.Text = "Add";
+            this.rbtnAdd.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSearch
+            // 
+            this.rbtnSearch.AutoSize = true;
+            this.rbtnSearch.Location = new System.Drawing.Point(20, 65);
+            this.rbtnSearch.Name = "rbtnSearch";
+            this.rbtnSearch.Size = new System.Drawing.Size(59, 17);
+            this.rbtnSearch.TabIndex = 2;
+            this.rbtnSearch.TabStop = true;
+            this.rbtnSearch.Text = "Search";
+            this.rbtnSearch.UseVisualStyleBackColor = true;
+            // 
+            // rbtnViewAll
+            // 
+            this.rbtnViewAll.AutoSize = true;
+            this.rbtnViewAll.Location = new System.Drawing.Point(20, 89);
+            this.rbtnViewAll.Name = "rbtnViewAll";
+            this.rbtnViewAll.Size = new System.Drawing.Size(59, 17);
+            this.rbtnViewAll.TabIndex = 3;
+            this.rbtnViewAll.TabStop = true;
+            this.rbtnViewAll.Text = "ViewAll";
+            this.rbtnViewAll.UseVisualStyleBackColor = true;
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.BackColor = System.Drawing.Color.Transparent;
+            this.btnContinue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnContinue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnContinue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContinue.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinue.Location = new System.Drawing.Point(144, 301);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(129, 54);
+            this.btnContinue.TabIndex = 3;
+            this.btnContinue.Text = "Continue";
+            this.btnContinue.UseVisualStyleBackColor = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 478);
+            this.Controls.Add(this.panel2);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnStudent;
+        private System.Windows.Forms.RadioButton rbtnModule;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbtnViewAll;
+        private System.Windows.Forms.RadioButton rbtnSearch;
+        private System.Windows.Forms.RadioButton rbtnAdd;
+        private System.Windows.Forms.RadioButton rbtnModify;
+    }
+}
---
+++ DataSmart/DataSmart/Presentation Layer/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSmart.Presentation_Layer
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void rbtnModule_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
